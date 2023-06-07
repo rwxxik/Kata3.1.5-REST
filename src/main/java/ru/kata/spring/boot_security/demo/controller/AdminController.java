86e495b814f159c41d868cfb5d443ca6f13db74d
a <rwxxik@gmail.com>
2023-06-07 22:19:10 +0300

    header for /user and /admin

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,12 +1,15 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.security.MyUserDetails;
 import ru.kata.spring.boot_security.demo.service.MyUserService;
 
 import javax.validation.Valid;
@@ -22,7 +25,10 @@ public class AdminController {
     }
 
     @GetMapping()
-    public String getAllUsersCRUD(ModelMap model) {
+    public String adminPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MyUserDetails userDetails = (MyUserDetails) authentication.getPrincipal();
+        model.addAttribute("user", userDetails.getUser());
         model.addAttribute("users", userService.getAllUsers());
         return "indexCRUD";
     }
